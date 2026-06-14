b771a397c5a2d9878e713a2209a4d01e5df9855c
YC-910 <144610099+YC-910@users.noreply.github.com>
2026-06-14 17:14:17 +0800

    Update website.py

--- website.py
+++ website.py
@@ -19,8 +19,10 @@ scope = [
     "https://www.googleapis.com/auth/drive"
 ]
 
-creds = Credentials.from_service_account_file(
-    "credentials.json",
+
+
+creds = Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
     scopes=scope
 )
 
